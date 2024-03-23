d280a9a3054a03f067f3ab7f018f4233445d41c4
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-03-23 17:43:43 +0000

    Add new views to Blog

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def starting_page(request):
+    return 
+
+def posts(request):
+    return
+
+def posts_details(request):
+    return
