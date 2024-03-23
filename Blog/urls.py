fe98bcafa79c079ee5889246095b7e21d60798c8
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-03-23 16:46:51 +0000

    Adding urls.py to the repository

---
+++ Blog/urls.py
@@ -0,0 +1,8 @@
+from django import path
+from . import views
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.posts_details, name="post-details-page")
+]
